3d0da4c8479dbde107c43c19ad1fa324d30434cf
Leyuur <yuri.vduarte@hotmail.com>
2024-07-31 15:22:47 -0300

    a

--- js/login.js
+++ js/login.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-analytics.js";
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -9,6 +10,7 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.4/firebas
 const firebaseConfig = {
   apiKey: "AIzaSyDzSdcJMfq0iLwVeJ-YINYGWVUo-3z-Qrw",
   authDomain: "youpi-49791.firebaseapp.com",
+  databaseURL: "https://youpi-49791-default-rtdb.firebaseio.com",
   projectId: "youpi-49791",
   storageBucket: "youpi-49791.appspot.com",
   messagingSenderId: "584506286648",
@@ -19,3 +21,25 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+const provider = new GoogleAuthProvider();
+const auth = getAuth();
+
+document.getElementById('google-btn');addEventListener('click', () => {
+  signInWithPopup(auth, provider).then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+
+      console.log(user);
+
+      
+    }).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.customData.email;
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+})
